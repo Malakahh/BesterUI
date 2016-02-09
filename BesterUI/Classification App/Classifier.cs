d25d86254189d49239b26170d52279a5a2146d37
BenjaBobs <bhuber11@student.aau.dk>
2016-02-09 13:34:20 +0100

    Added libsvm

---
+++ BesterUI/Classification App/Classifier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classification_App
+{
+    class Classifier
+    {
+
+    }
+}
