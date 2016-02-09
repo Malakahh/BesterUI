bc8600e97356c8f998d154b2dd14d7dcb811bd12
Anders Bender <abende11@student.aau.dk>
2016-02-09 14:27:18 +0100

    Add Classifier base class

---
+++ BesterUI/Classification App/StdClassifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classification_App
+{
+    class StdClassifier : Classifier
+    {
+        public StdClassifier(string Name) : base(Name)
+        {
+        }
+
+        protected override void CrossValidate(List<double> CTypes, List<double> GTypes, bool RunCombinations)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override void PrintResults()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
