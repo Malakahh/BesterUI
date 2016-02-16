2896e5acaa59c5837cff5ef28005ecbf3c1b141b
Anders Bender <abende11@student.aau.dk>
2016-02-16 13:25:35 +0100

    Added emotiv library

--- BesterUI/BesterUI/DataCollectors/EEGCollector.cs
+++ BesterUI/BesterUI/DataCollectors/EEGCollector.cs
@@ -9,9 +9,26 @@ namespace BesterUI.DataCollectors
 {
     class EEGCollector
     {
+
+        private EmoEngine eegEngine;
+
         public EEGCollector()
+        {
+            eegEngine = EmoEngine.Instance;
+        }
+
+        public void Connect()
+        {
+            eegEngine.UserAdded += EegEngine_UserAdded;
+            eegEngine.Connect();
+        }
+
+        #region [Events]
+        private void EegEngine_UserAdded(object sender, EmoEngineEventArgs e)
         {
 
         }
+        #endregion
+
     }
 }
