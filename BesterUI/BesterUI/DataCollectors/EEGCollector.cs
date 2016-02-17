96cd0dfadccf7a8fda3d7914440051d69541e9d0
Anders Bender <abende11@student.aau.dk>
2016-02-17 10:37:41 +0100

    EEGCollector tested and reworked

--- BesterUI/BesterUI/DataCollectors/EEGCollector.cs
+++ BesterUI/BesterUI/DataCollectors/EEGCollector.cs
@@ -6,6 +6,7 @@ using System.Text;
 using Emotiv;
 using System.Threading.Tasks;
 using BesterUI.Data;
+using BesterUI.Helpers;
 
 namespace BesterUI.DataCollectors
 {
@@ -32,24 +33,32 @@ namespace BesterUI.DataCollectors
         volatile bool shouldCollectData = true;
         private void Collect()
         {
+            int iterations = 0;
+            int dataNotReceived = 0;
+            Log.LogMessage("EEG data collection started");
             Dictionary<EdkDll.EE_DataChannel_t, double[]> input;
             while (shouldCollectData)
             {
                 // Handle any waiting events
                 eegEngine.ProcessEvents();
-
+                
                 if (userID == -1)
                 {
-                    return;
+                    iterations++;
+                    Log.LogMessageSameLine("No users found! number of times tried: " + iterations);
+                    Thread.Sleep(100);
+                    continue;
                 }
-
+                
                 input = eegEngine.GetData((uint)userID);
-
+                
                 if (input == null)
                 {
-                    return;
+                    Log.LogMessageSameLine("No data receied number of times: " + ++dataNotReceived);
+                    Thread.Sleep(100);
+                    continue;
                 }
-
+                
                 double max = input[EdkDll.EE_DataChannel_t.TIMESTAMP].Max();
                 for (int j = (int)EdkDll.EE_DataChannel_t.AF3; j <= (int)EdkDll.EE_DataChannel_t.AF4; j++)
                 {
@@ -65,8 +74,9 @@ namespace BesterUI.DataCollectors
                     }
                 }
 
+                Log.LogMessage(input[0].Length);
                 input = null;
-
+                
                 Thread.Sleep(100);
             }
 
@@ -75,9 +85,16 @@ namespace BesterUI.DataCollectors
 
         public void StartCollect()
         {
-
+            dataCollectThread = null;
             dataCollectThread = new Thread(Collect);
             dataCollectThread.Start();
+        }
+
+        public void StopCollect()
+        {
+            GSRDataReading.StaticEndWrite("EEG");
+            Log.LogMessage("EEG data Collection stopped");
+            shouldCollectData = false;
         }
 
         #region [Events]
