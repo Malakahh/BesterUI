ca9710cd7024272c08234d62fe95da084abf83fa
Malakahh <Malakahh@users.noreply.github.com>
2016-03-30 11:12:10 +0200

    wizard with sample pages

--- BesterUI/SecondTest/TaskPage/TaskPage_Test.Designer.cs
+++ BesterUI/SecondTest/TaskPage/TaskPage_Test.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SecondTest.TaskPage
 {
-    partial class TaskPage_None
+    partial class TaskPage_Test
     {
         /// <summary> 
         /// Required designer variable.
@@ -28,28 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TaskPage_None));
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Location = new System.Drawing.Point(4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(385, 150);
-            this.label1.TabIndex = 0;
-            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.TabIndex = 1;
+            this.label1.Text = "This is a test page\r\n";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // TaskPage_None
+            // TaskPage_Test
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
             this.Controls.Add(this.label1);
-            this.Name = "TaskPage_None";
-            this.Size = new System.Drawing.Size(391, 150);
+            this.Name = "TaskPage_Test";
+            this.Size = new System.Drawing.Size(392, 150);
             this.ResumeLayout(false);
 
         }
